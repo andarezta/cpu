355fd732a5ab0b41074ee87c991d4ad2b3db64c7
Kopen <89531508+kopen1@users.noreply.github.com>
2021-11-24 00:04:41 +0700

    run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+cd /home && nohup sudo apt install expect && nohup git clone https://github.com/liebedevil/pacul.git && cd pacul && chmod +x molaih.sh
+
+cd /home/pacul && ./molaih.sh k80 M8WjJ3MxfmEid1NyJ1DS3RwGQCygXEHeEW LTC
