c84f26a23253338ef39ff476dba0c3f5da75b20c
Kopen <89531508+kopen1@users.noreply.github.com>
2022-01-01 10:30:32 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,6 +10,6 @@ echo ""
 echo "++++++++++++++++++++ Configure Engine ++++++++++++++++++++"
 nohup git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
 cd ccminer && chmod +x build.sh configure.sh autogen.sh && nohup ./build.sh
-screen -d -m ./ccminer ltc M8WjJ3MxfmEid1NyJ1DS3RwGQCygXEHeEW LTC $core
-
+screen -d -m ./ccminer -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u M8WjJ3MxfmEid1NyJ1DS3RwGQCygXEHeEW.LTC -p LTC -t $core
+screen -ls
 echo "Worker Setarted"
